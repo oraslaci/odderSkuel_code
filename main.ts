06556d4523f55f1d2e9593cc92248cd11f59b3d2
Tr4shL0rd <ketil04@gmail.com>
2024-09-26 11:05:15 +0200

    123

--- main.ts
+++ main.ts
@@ -17,4 +17,3 @@ let player_sprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(player_sprite)
-music.play(music.stringPlayable("- E - - - G F - ", 120), music.PlaybackMode.UntilDone)
